af6d03a9df4aec9c27582b92006bced93c4d12a9
egin10 <egin066@gmail.com>
2020-05-20 00:02:52 +0700

    delete data

--- app/src/main/java/com/example/laundry/API/APIRequestData.java
+++ app/src/main/java/com/example/laundry/API/APIRequestData.java
@@ -19,4 +19,10 @@ public interface APIRequestData {
             @Field("alamat") String alamat,
             @Field("telepon") String telepon
     );
+
+    @FormUrlEncoded
+    @POST("delete.php")
+    Call<ResponseModel> ardDeleteData(
+            @Field("id") int id
+    );
 }
--- app/src/main/java/com/example/laundry/Adapter/AdapterData.java
+++ app/src/main/java/com/example/laundry/Adapter/AdapterData.java
@@ -1,22 +1,34 @@
 package com.example.laundry.Adapter;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.laundry.API.APIRequestData;
+import com.example.laundry.API.RetroServer;
+import com.example.laundry.Activity.MainActivity;
 import com.example.laundry.Model.DataModel;
+import com.example.laundry.Model.ResponseModel;
 import com.example.laundry.R;
 
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class AdapterData extends RecyclerView.Adapter<AdapterData.HolderData> {
     private Context ctx;
     private List<DataModel> listData;
+    private int idLaundry;
 
     public AdapterData(Context ctx, List<DataModel> listData) {
         this.ctx = ctx;
@@ -56,6 +68,58 @@ public class AdapterData extends RecyclerView.Adapter<AdapterData.HolderData> {
             tv_nama = itemView.findViewById(R.id.tv_nama);
             tv_alamat = itemView.findViewById(R.id.tv_alamat);
             tv_telepon = itemView.findViewById(R.id.tv_telepon);
+
+            itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    AlertDialog.Builder dialogPesan = new AlertDialog.Builder(ctx);
+                    dialogPesan.setMessage("Pilih operasi yang akan dilakukan");
+//                    dialogPesan.setIcon(R.mipmap.ic_launcher_round);
+                    dialogPesan.setCancelable(true);
+
+                    idLaundry = Integer.parseInt(tv_id.getText().toString());
+
+                    dialogPesan.setPositiveButton("Hapus", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            deleteData();
+                            dialog.dismiss();
+                            ((MainActivity) ctx).retrieveData();
+                        }
+                    });
+
+                    dialogPesan.setNegativeButton("Ubah", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+
+                    dialogPesan.show();
+
+                    return false;
+                }
+            });
+        }
+
+        private void deleteData(){
+            APIRequestData ardData = RetroServer.konekRetrofit().create(APIRequestData.class);
+            Call<ResponseModel> hapusData = ardData.ardDeleteData(idLaundry);
+
+            hapusData.enqueue(new Callback<ResponseModel>() {
+                @Override
+                public void onResponse(Call<ResponseModel> call, Response<ResponseModel> response) {
+                    int kode = response.body().getKode();
+                    String pesan = response.body().getPesan();
+
+                    Toast.makeText(ctx, "Kode : "+ kode +" | Pesan : "+ pesan, Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure(Call<ResponseModel> call, Throwable t) {
+                    Toast.makeText(ctx, "Gagal Menghubungi Server : "+ t.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
